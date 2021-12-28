e589549c3ddce921792deba68fdfed5aae3d23e5
Erika Lopes <erika.calu@tc.com.br>
2021-12-28 14:12:10 -0300

    Add sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,10 @@
-import Head from 'next/head'
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
-    <div className="">
-      <Head>
-        <title>Spotify with Next</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <h1>Spotify 2.0</h1>
-      <main>
-        {/** Sidebar */}
+    <div className="bg-black h-screen overflow-hidden">
+      <main className="">
+        <Sidebar />{/** Sidebar */}
         {/** Center */}
       </main>
       <div>
